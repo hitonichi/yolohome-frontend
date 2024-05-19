b9c0f8c22358fa7bc2969ac922ef2ee2e7fd2b6d
hitonichi <42115291+hitonichi@users.noreply.github.com>
2024-05-19 15:23:05 +0700

    chore: remove legacy files

--- src/components/sidebar-layout/footer.tsx
+++ src/components/sidebar-layout/footer.tsx
@@ -1,11 +1,9 @@
 import Logo from '../logo';
-import Newsletter from '../newsletter';
 import { Socials } from './socials';
 
 const Footer = () => {
   return (
     <footer className="w-full pb-20 ">
-      <Newsletter />
       <div className="max-w-[1280px] w-[90%]  p-8 mx-auto h-full flex flex-col items-center justify-center">
         <div className="flex flex-col gap-2 items-center">
           <Logo size={64} className="mt-6" />
