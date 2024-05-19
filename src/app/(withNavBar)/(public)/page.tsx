17936b30c79a35e527ea0661abfcfd7f8f34f659
hitonichi <42115291+hitonichi@users.noreply.github.com>
2024-05-19 16:10:12 +0700

    chore: update navbar

--- src/app/(withNavBar)/(public)/page.tsx
+++ src/app/(withNavBar)/(public)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div className="w-full flex flex-col gap-0 justify-center items-center">This is home page</div>
+      <div className="w-full min-h-[100vh] flex flex-col gap-0 justify-center items-center">This is home page</div>
     </>
   );
 }
