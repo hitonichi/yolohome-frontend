ec3bf6b97a376f900c48b75ebd08f22dfdb26efe
hitonichi <42115291+hitonichi@users.noreply.github.com>
2024-05-24 13:03:44 +0700

    feat: update dash board - Add feeds to quick actions - Change sensors to News

--- src/app/(withNavBar)/(protected)/dashboard/page.tsx
+++ src/app/(withNavBar)/(protected)/dashboard/page.tsx
@@ -1,21 +1,87 @@
-import DashboardSensors from '@/components/dashboard/dashboardSensor';
+import { FeedInfo } from '@/app/types/feed';
+import News from '@/components/dashboard/News';
+import QuickActions from '@/components/dashboard/QuickActions';
 import { Label } from '@/components/ui/label';
-import { Icon } from '@radix-ui/react-select';
 import { Expand } from 'lucide-react';
 import { FC } from 'react';
+
+// const MOCK_FEEDS: FeedInfo[] = [
+//   {
+//     id: 2799243,
+//     name: 'pihome-temperature',
+//     description: '',
+//     created_at: '2024-05-03T09:13:38Z',
+//     key: 'pihome-temperature',
+//     last_value: '29.4',
+//     last_value_at: '2024-05-03T09:40:14Z',
+//   },
+//   {
+//     id: 2799244,
+//     name: 'pihome-humidity',
+//     description: '',
+//     last_value: '52.5',
+//     last_value_at: '2024-05-03T09:40:14Z',
+//     created_at: '2024-05-03T09:13:38Z',
+//     key: 'pihome-humidity',
+//   },
+//   {
+//     id: 2799245,
+//     name: 'pihome-lux',
+//     description: '',
+//     last_value: '58',
+//     last_value_at: '2024-05-03T09:40:14Z',
+//     created_at: '2024-05-03T09:13:38Z',
+//     key: 'pihome-lux',
+//   },
+//   {
+//     id: 2814728,
+//     name: 'pihome-light',
+//     description: '',
+//     last_value: null,
+//     last_value_at: null,
+//     license: null,
+//     created_at: '2024-05-22T07:42:42Z',
+//     key: 'pihome-light',
+//     group: {
+//       id: 1174997,
+//       key: 'default',
+//       name: 'Default',
+//       description: 'This is your default group. It contains all feeds not otherwise grouped.',
+//       created_at: '2024-04-09T01:38:31Z',
+//     },
+//   },
+//   {
+//     id: 2814729,
+//     name: 'pihome-fan',
+//     description: '',
+//     last_value: null,
+//     last_value_at: null,
+//     license: null,
+//     created_at: '2024-05-22T07:42:49Z',
+//     key: 'pihome-fan',
+//     group: {
+//       id: 1174997,
+//       key: 'default',
+//       name: 'Default',
+//       description: 'This is your default group. It contains all feeds not otherwise grouped.',
+//       created_at: '2024-04-09T01:38:31Z',
+//     },
+//   },
+// ];
 
 export default function DashboardPage() {
   return (
     <div className="relative w-full h-full grid grid-cols-12 grid-rows-1 gap-4">
       <div className="grid grid-rows-6 row-span-1 col-span-8 gap-4">
-        <DashboardCard title="Quick actions" className="row-span-4"></DashboardCard>
+        <DashboardCard title="Quick actions" className="row-span-4">
+          <QuickActions />
+        </DashboardCard>
         <DashboardCard title="History" className="row-span-2"></DashboardCard>
       </div>
-      <div className="grid grid-rows-6 row-span-1 col-span-4 gap-4">
-        <DashboardCard title="Sensors" className="row-span-3">
-          <DashboardSensors />
+      <div className="grid grid-rows-12 row-span-1 col-span-4 gap-4">
+        <DashboardCard title="News" className="row-span-12">
+          <News />
         </DashboardCard>
-        <DashboardCard title="Graphs" className="row-span-3"></DashboardCard>
       </div>
     </div>
   );
@@ -30,15 +96,16 @@ type DashboardCardProps = {
 const DashboardCard: FC<DashboardCardProps> = ({ title, description, className, children }) => {
   const twClasses =
     'h-full flex flex-col outline outline-primary/20 outline-1 hover:bg-primary/5 hover:shadow-lg hover:outline-none w-full h-full rounded p-2';
+
   return (
     <div className={[twClasses, className].join(' ')}>
-      <div className="w-full flex justify-between mb-2">
-        <Label className="font-bold text-md">{title}</Label>
+      <div className="w-full flex justify-between mt-1 mb-3">
+        <Label className="font-bold text-md ml-2">{title}</Label>
         <div className="cursor-pointer bg-gray-200 hover:bg-gray-300 w-6 h-6 rounded-full flex justify-center items-center">
           <Expand size={16} className="text-gray-500" />
         </div>
       </div>
-      <div className="relative flex-1">{children}</div>
+      <div className="relative flex-1  overflow-y-scroll no-scrollbar">{children}</div>
     </div>
   );
 };
--- src/components/dashboard/dashboardSensor/index.tsx
+++ src/components/dashboard/dashboardSensor/index.tsx
@@ -1,4 +1,9 @@
+'use client';
+
+import { useFeeds } from '@/app/hooks/useFeeds';
+import { FeedInfo, FeedSensorUnit } from '@/app/types/feed';
 import { Label } from '@/components/ui/label';
+import { extractFeedType } from '@/lib/utils';
 
 const SENSORS = [
   {
@@ -33,13 +38,33 @@ const SENSORS = [
   },
 ];
 
+export type DashboardSensorsProps = {
+  feeds: FeedInfo[];
+};
 const DashboardSensors = () => {
+  const { data, error, isPending } = useFeeds();
+
+  if (isPending) return <div>Loading...</div>;
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  // Type assert here
+  const feeds = data as FeedInfo[];
+  console.log('DATA', feeds);
+  // if (data) {
+  // }
+
   return (
-    <div className="h-full flex flex-col gap-2 overflow-y-scroll no-scrollbar">
-      {SENSORS.map((sensor, id) => (
-        <div className="bg-white shadow rounded p-2 flex items-center justify-between" key={id}>
-          <Label className="font-bold">{sensor.name}</Label>
-          <p className="text-primary text-3xl font-bold">{sensor.value}</p>
+    <div className="flex flex-col gap-2">
+      {feeds.map((sensor, id) => (
+        <div className="bg-white shadow rounded py-2 px-3 flex items-center justify-between gap-2" key={id}>
+          <Label className="text-lg">{sensor.name}</Label>
+          <p className="flex-1 flex justify-end text-primary text-3xl font-bold">{sensor.last_value}</p>
+          <p className="min-w-[10%] flex justify-end items-end text-2xl text-gray-400">
+            {FeedSensorUnit[extractFeedType(sensor.name)]}
+          </p>
         </div>
       ))}
     </div>
